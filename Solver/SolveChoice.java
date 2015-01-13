dda1ab2116e896651d46f08bf89690d58d83d47b
rgw3d <rgw3d@aol.com>
2015-01-13 12:43:34 -0500

    Adding method descriptions in the SolveChoice class

--- Solver/SolveChoice.java
+++ Solver/SolveChoice.java
@@ -8,14 +8,21 @@ import java.util.ArrayList;
 
 /**
  * Created by rgw3d on 1/13/2015.
- * This class will determine what action is required to solve an equation for x
+ * This class will determine what action is required to solve an equation for its variable
  */
 public class SolveChoice {
 
+    /**
+     * Starts the process of trying to solve for the variable
+     *
+     * @param list the list of EquationNode objects that hold the variables/numerical values.  this list
+     *             should just be NumberStructure objects, Nominal and Fraction
+     */
     public static void startSolve(ArrayList<EquationNode> list){
         int length = list.size();
-        boolean fractions = findFractions(list);
+        boolean hasFractions = findFractions(list);//if true, then it is not a true polynomial
         double highestExponent = findHighestExponent(list);
+
     }
 
     /**
